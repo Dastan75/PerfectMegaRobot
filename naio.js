7727c680162d5ba36ea9bda2e5fbe2acc69f6de0
fabienh <fabien@egosventures.com>
2017-11-28 01:27:56 +0100

    update

--- naio.js
+++ naio.js
@@ -1,8 +1,9 @@
 const			net						= require('net');
+const           jspack				= require('jspack');
 var				lidarData			= "";
 var				gyroData			= "";
-const			IP						= '127.0.0.1';
-// const			IP						= '192.168.1.95';
+// const			IP						= '127.0.0.1';
+const			IP						= '192.168.1.95';
 var				trameMotor		= '\x4e' + '\x41' + '\x49' + '\x4f' + '\x30' + '\x31' + '\x01' + '\x00' + '\x00' + '\x00' + '\x02' + '\x7f' + '\x7f' + '\x00' + '\x00' + '\x00' + '\x00';
 var connectIndex = 0;
 
@@ -23,6 +24,7 @@ function nconnect(HOST, PORT) {
 	    connectIndex ++;
 	    if(connectIndex == 3) {
 	    	main();
+	    	console.log('========================================================================================');
 	    }
 	});
 	client.on('error', function() {
@@ -47,9 +49,12 @@ function nlistenGyro(client) {
 function nlistenLidar(client) {
 	client.on('data', function(data) {
 		console.log('receive lidar data')
+
 		// console.log(hex2a(data));
 		// console.log("lydar", data)
+		// lidarData = hex2a(data);
 		lidarData = data;
+		// getLidarData();
 	});
 }
 
@@ -99,245 +104,264 @@ function main() {
 	var degs = 0;
 	var dataDistanceD = 0;
 	var dataDistanceG = 0;
+	var isPaused = false
 
 	setInterval(function() {
-		// Gps = Com.Gps()
-		// if(all((Value == 4000)for Value in data)):
-			// Virage3t()
-		lidarData = getLidarData();
+		if(!isPaused) {
+			// Gps = Com.Gps()
+			// if(all((Value == 4000)for Value in data)):
+				// Virage3t()
+			lidarData = getLidarData();
 
-		if ((lidarData != oldLidarData) && (init != 1)) {
-			console.log("Obstacle Détecté")
-			indexArray = [];
-			distanceArray = [];
-			calcArray = [];
-			alignArray1 = [];
-			alignArray2 = [];
-			tempArray = [];
-			tempArray2 = [];
-
-			for(var i = 0; i < lidarData.length; i += 1) {
-				if(lidarData[i] != oldLidarData[i]) {
-					indexArray.push(i)
-					distanceArray.push(lidarData[i])
+			if ((lidarData != oldLidarData) && (init != 1)) {
+				console.log("Obstacle Détecté")
+				indexArray = [];
+				distanceArray = [];
+				calcArray = [];
+				alignArray1 = [];
+				alignArray2 = [];
+				tempArray = [];
+				tempArray2 = [];
+				console.log('lidarData.length')
+				console.log(lidarData.length)
+				for(var i = 0; i < lidarData.length; i += 1) {
+					if(lidarData[i] != oldLidarData[i]) {
+						console.log(i);
+						// console.log(lidarData[i]);
+						indexArray.push(i)
+						distanceArray.push(lidarData[i])
+					}
 				}
-			}
 
-			for(var i = 0; i < indexArray.length; i += 1) {
-				calcArray.push(Math.round(Math.cos(Math.radians(indexArray[i]))*distanceArray[i]))
-				//console.log("lstindex : ",lstIndex[i])
-				//console.log("lstdistance : ",lstDistance[i])
-				//console.log("lstcalc : ",lstCalc[i])
+				for(var i = 0; i < indexArray.length; i += 1) {
+					calcArray.push(Math.round(Math.cos(Math.radians(indexArray[i]))*distanceArray[i]))
+					//console.log("lstindex : ",lstIndex[i])
+					//console.log("lstdistance : ",lstDistance[i])
+					//console.log("lstcalc : ",lstCalc[i])
 
-			}
+				}
 
-			for(var i = 0; i < calcArray.length; i += 1) {
-				for(var j = 0; j < calcArray.length; j += 1) {
-					if ((calcArray[i] == calcArray[j])
-						&& (j != i)
-						&& (calcArray[j] != 0)
-						&& (calcArray[i] != 0)
-						) {
-						result = alignArray1.indexOf(calcArray[j]);
-						if (result == -1) {
-							alignArray1.push(calcArray[j])
-							tmp = []
-							tmp.push(indexArray[j])
-							alignArray2.push(tmp)
-						} else {
-							result2 = alignArray2[result].indexOf(indexArray[j]);
-							if (result2 == -1) {
-								alignArray2[result].push(indexArray[j])
+				// console.log(calcArray)
+				console.log('distanceArray');
+				console.log(distanceArray)
+				console.log('calcArray')
+				console.log(calcArray)
+				console.log('indexArray')
+				console.log(indexArray)
+
+				for(var i = 0; i < calcArray.length; i += 1) {
+					for(var j = 0; j < calcArray.length; j += 1) {
+						if ((calcArray[i] == calcArray[j])
+							&& (j != i)
+							&& (calcArray[j] != 0)
+							&& (calcArray[i] != 0)
+							) {
+							result = alignArray1.indexOf(calcArray[j]);
+							if (result == -1) {
+								alignArray1.push(calcArray[j])
+								tmp = []
+								tmp.push(indexArray[j])
+								alignArray2.push(tmp)
+							} else {
+								result2 = alignArray2[result].indexOf(indexArray[j]);
+								if (result2 == -1) {
+									alignArray2[result].push(indexArray[j])
+								}
 							}
 						}
 					}
 				}
-			}
 
-			console.log('alignArray2')
-			console.log(alignArray2)
+				console.log('alignArray2')
+				console.log(alignArray2)
 
-			var alignArray = [];
-			var maxLigne;
-			alignArray = removeDuplicates(alignArray1)
-			size = 0
+				var alignArray = [];
+				var maxLigne;
+				alignArray = removeDuplicates(alignArray1)
+				size = 0
 
-			for(var i = 0; i < alignArray2.length; i += 1) {
-				size += alignArray2[i].length;
-				tempArray.push(alignArray2[i].length);
-			}
-			if ((alignArray2.length) != 0) {
-				average = Math.floor(size / alignArray2.length);
-			}
-			//console.log ("Avg", average)
-			console.log('tempArray')
-			console.log(tempArray)
-			if (tempArray.length > 0) {
-				// maxLigne = Math.max(tempArray);
-				var maxLigne = tempArray.reduce(function(a,b) {
-				  return Math.max(a, b);
-				});
-			}
-			tempArray = [];
-			w = 1;
-			distanceD = 0;
-			distanceG = 0;
-			while (w == 1) {
-				w = 0
-				for (var i = 0; i < alignArray2.length; i += 1) {
-					//console.log ("Size : ", len(lstAlign2[j]), ", IT : ", j)
-					if ((alignArray2[i].length) < average) {
-						alignArray1.splice(i, 1);
-						alignArray2.splice(i, 1);
-						w = 1
-						break
-					}
+				console.log(alignArray2)
+
+				for(var i = 0; i < alignArray2.length; i += 1) {
+					size += alignArray2[i].length;
+					tempArray.push(alignArray2[i].length);
 				}
-			}
-
-			alignArray1.push(4000)
-			alignArray1.push(-4000)
-			//Correction Trajectoire
-			console.log(alignArray1)
-			for(index in alignArray1) {
-				var value = alignArray1[index];
-				if(value < 0) {
-					tempArray.push(Math.abs(value))
-					// console.log('existDroite');
-				} else if (value > 0) {
-					tempArray2.push(value)
-					// console.log('existGauche')
+				if ((alignArray2.length) != 0) {
+					average = Math.floor(size / alignArray2.length);
 				}
-			}
-
-
-			// distanceD = Math.min(tempArray);
-			var distanceD = tempArray.reduce(function(a,b) {
-			  return Math.min(a, b);
-			});
-
-			if (distanceD > 1000) {
-				distanceD = 0
-			}
-
-			// distanceG = Math.min(tempArray2)
-			var distanceG = tempArray2.reduce(function(a,b) {
-			  return Math.min(a, b);
-			});
-
-			if (distanceG > 1000) {
-				distanceG = 0
-			}
-			if ((distanceD != 0) && (distanceG != 0)) {
-				invRota = 0
-			}
-
-			checkForLignes(distanceD, distanceG, 'droite'); //checkLign
-			checkForLignes(distanceG, distanceD, 'gauche');
-
-			function checkForLignes(distance1, distance2, checkDirection) {
-				if((distance1 == 0) && (distance2 > 0) && (distance2 < 700)) {
-					console.log("Pas de ligne " + checkDirection)
-					distance1 = distance2
-					if(invRota == 1) {
-						invRota = 2
-					}
-
-					if(saveOneRange == -1) {
-						saveOneRange = distance2;
-					} else {
-						distance1 = saveOneRange
-					}
+				//console.log ("Avg", average)
+				console.log('tempArray')
+				console.log(tempArray)
+				if (tempArray.length > 0) {
+					// maxLigne = Math.max(tempArray);
+					var maxLigne = tempArray.reduce(function(a,b) {
+					  return Math.max(a, b);
+					});
 				}
-			}
-
-			//Sequence d'instruction Motors
-
-			console.log("Droite : " + distanceD)
-			console.log("Gauche : " + distanceG)
-			console.log("dataDistanceD : " + dataDistanceD)
-			console.log("dataDistanceG : " + dataDistanceG)
-			console.log("maxLigne : " + maxLigne)
-			console.log("recentrage : " + recentrage)
-
-			if ((recentrage == 1) && (maxLigne >= 12)) {
-				recentrage = 0
-			}
-			if ((recentrage == 1) && (maxLigne < 12)) {
-				console.log("Recentrage aprés virage")
-				for(var i = 0; i < 10; i += 1) {
-					if(nextVirage == 'droite') {
-						turnForwardLeft();
-					} else {
-						turnForwardRight();
-					}
-				}
-			} else if (((distanceD + distanceG) / 2) > distanceG) {
-				console.log("Correction vers la droite")
-				turnForwardRight()
-				angleCor = angleCor + 0.3
-			} else if (((distanceD + distanceG) / 2) > distanceD) {
-				console.log("Correction vers la gauche")
-				turnForwardLeft()
-				angleCor = angleCor - 0.3
-			} else if ((distanceD == dataDistanceD) && (distanceG == dataDistanceG)) {
-				console.log("Le robot semble au milieu")
-			} else {
-				console.log("Probleme de correction de trajectoire")
-			}
-
-			dataDistanceD = distanceD;
-			dataDistanceG = distanceG;
-			if ((distanceD == 0) && (distanceG == 0)) {
-				console.log("Pas de ligne d'avancement detecté")
-				if(recentrage == 0) {
-					//Inversion de la Rotation
-					if(invRota == 2) {
-						if(nextVirage == 'droite') {
-							nextVirage = 'gauche';
-						} else if (nextVirage == 'gauche') {
-							nextVirage = 'droite';
+				tempArray = [];
+				w = 1;
+				distanceD = 0;
+				distanceG = 0;
+				while (w == 1) {
+					w = 0
+					for (var i = 0; i < alignArray2.length; i += 1) {
+						//console.log ("Size : ", len(lstAlign2[j]), ", IT : ", j)
+						if ((alignArray2[i].length) < average) {
+							alignArray1.splice(i, 1);
+							alignArray2.splice(i, 1);
+							w = 1
+							break
 						}
 					}
-					//Rotation
-					Virage3t(nextVirage)
 				}
-			} else if (indexArray.every(isOverNinety)) {
-				console.log(indexArray)
-				console.log("Obstacle uniquement sur la droite")
-				moveForward()
-			} else if (indexArray.every(isBelowNinety)) {
-				console.log(indexArray)
-				console.log("Obstacle uniquement sur la gauche")
-				moveForward()
-			} else if (indexArray.every(isBetweenSixtyAndOneTwenty)) {
-				console.log("Obstacle sur trajectoire")
-			} else {
-				moveForward()
-				// Gpstemp = Gps
-				oldLidarData = lidarData;
-			}
+
+				alignArray1.push(4000)
+				alignArray1.push(-4000)
+				//Correction Trajectoire
+				console.log(alignArray1)
+				for(index in alignArray1) {
+					var value = alignArray1[index];
+					if(value < 0) {
+						tempArray.push(Math.abs(value))
+						// console.log('existDroite');
+					} else if (value > 0) {
+						tempArray2.push(value)
+						// console.log('existGauche')
+					}
+				}
 
 
-		// else if(Gps == Gpstemp):
-			// console.log("Robot embourbé, tentative de désembourbage")
-			// moveForward()
-			// Gps = Com.Gps()
-			// if(Gps == Gpstemp):
-				// Com.Reculer()
+				// distanceD = Math.min(tempArray);
+				var distanceD = tempArray.reduce(function(a,b) {
+				  return Math.min(a, b);
+				});
+
+				if (distanceD > 1000) {
+					distanceD = 0
+				}
+
+				// distanceG = Math.min(tempArray2)
+				var distanceG = tempArray2.reduce(function(a,b) {
+				  return Math.min(a, b);
+				});
+
+				if (distanceG > 1000) {
+					distanceG = 0
+				}
+				if ((distanceD != 0) && (distanceG != 0)) {
+					invRota = 0
+				}
+
+				checkForLignes(distanceD, distanceG, 'droite'); //checkLign
+				checkForLignes(distanceG, distanceD, 'gauche');
+
+				function checkForLignes(distance1, distance2, checkDirection) {
+					if((distance1 == 0) && (distance2 > 0) && (distance2 < 700)) {
+						console.log("Pas de ligne " + checkDirection)
+						distance1 = distance2
+						if(invRota == 1) {
+							invRota = 2
+						}
+
+						if(saveOneRange == -1) {
+							saveOneRange = distance2;
+						} else {
+							distance1 = saveOneRange
+						}
+					}
+				}
+
+				//Sequence d'instruction Motors
+
+				console.log("Droite : " + distanceD)
+				console.log("Gauche : " + distanceG)
+				console.log("dataDistanceD : " + dataDistanceD)
+				console.log("dataDistanceG : " + dataDistanceG)
+				console.log("maxLigne : " + maxLigne)
+				console.log("recentrage : " + recentrage)
+
+				if ((recentrage == 1) && (maxLigne >= 12)) {
+					recentrage = 0
+				}
+				if ((recentrage == 1) && (maxLigne < 12)) {
+					console.log("Recentrage aprés virage")
+					for(var i = 0; i < 10; i += 1) {
+						if(nextVirage == 'droite') {
+							turnForwardLeft();
+						} else {
+							turnForwardRight();
+						}
+					}
+				} else if (((distanceD + distanceG) / 2) > distanceG) {
+					console.log("Correction vers la droite")
+					turnForwardRight()
+					angleCor = angleCor + 0.3
+				} else if (((distanceD + distanceG) / 2) > distanceD) {
+					console.log("Correction vers la gauche")
+					turnForwardLeft()
+					angleCor = angleCor - 0.3
+				} else if ((distanceD == dataDistanceD) && (distanceG == dataDistanceG)) {
+					console.log("Le robot semble au milieu")
+				} else {
+					console.log("Probleme de correction de trajectoire")
+				}
+
+				dataDistanceD = distanceD;
+				dataDistanceG = distanceG;
+				if ((distanceD == 0) && (distanceG == 0)) {
+					console.log("Pas de ligne d'avancement detecté")
+					if(recentrage == 0) {
+						//Inversion de la Rotation
+						if(invRota == 2) {
+							if(nextVirage == 'droite') {
+								nextVirage = 'gauche';
+							} else if (nextVirage == 'gauche') {
+								nextVirage = 'droite';
+							}
+						}
+						//Rotation
+						console.log('VEUT FAIRE Virage3t')
+						isPaused = true;
+						Virage3t(nextVirage);
+						isPaused = false;
+					}
+				} else if (indexArray.every(isOverNinety)) {
+					console.log(indexArray)
+					console.log("Obstacle uniquement sur la droite")
+					moveForward()
+				} else if (indexArray.every(isBelowNinety)) {
+					console.log(indexArray)
+					console.log("Obstacle uniquement sur la gauche")
+					moveForward()
+				} else if (indexArray.every(isBetweenSixtyAndOneTwenty)) {
+					console.log("Obstacle sur trajectoire")
+				} else {
+					moveForward()
+					// Gpstemp = Gps
+					oldLidarData = lidarData;
+				}
+
+
+			// else if(Gps == Gpstemp):
+				// console.log("Robot embourbé, tentative de désembourbage")
+				// moveForward()
 				// Gps = Com.Gps()
 				// if(Gps == Gpstemp):
-					// console.log("Echec du désembourbage, SOS SOS SOS")
-		} else {
-			moveForward();
-			// turnBackWardLeft();
-			console.log("Action Par Default");
-			init = 0;
-			oldLidarData = lidarData;
-			// Gpstemp = Gps;
-			// datatemp = data;
+					// Com.Reculer()
+					// Gps = Com.Gps()
+					// if(Gps == Gpstemp):
+						// console.log("Echec du désembourbage, SOS SOS SOS")
+			} else {
+				moveForward();
+				// turnBackWardLeft();
+				console.log("Action Par Default");
+				init = 0;
+				oldLidarData = lidarData;
+				// Gpstemp = Gps;
+				// datatemp = data;
+			}
+			// time.sleep(tps)
 		}
-		// time.sleep(tps)
 	}, 25);
 	return 0
 
@@ -372,9 +396,8 @@ function Virage3t(nextVirage) {
 		} else {
 			turnForwardLeft();
 		}
-		degs = getGyroData;
+		degs = getGyroData();
 		angle = angle + (degs);
-		i=i+1
 		console.log("Angle : " + angle)
 		// time.sleep(tps)
 	}
@@ -412,45 +435,57 @@ function moveForward() {
 
 function getLidarData() {
 	console.log('getLidarData')
-  var tmp = lidarData.slice(101, 467);
-  lidarData = tmp;
-	console.log(lidarData);
-	console.log(lidarData.length)
-	return lidarData;
+	// return lidarData;
+	// console.log(lidarData);
+	var tmp = lidarData.slice(103, 463);
+	// console.log(tmp)
+
+	// return tmp;
+	lidarData = tmp;
+	// console.log(lidarData);
+	// console.log(lidarData.length)
+	var ndata = [];
+	for(var i = 0; i < lidarData.length; i += 2) {
+		test = [lidarData[i]];
+		test2 = [lidarData[i+1]];
+		bytes = test.concat(test2);
+		// var x = new Uint16Array(bytes)
+		var ntest = jspack.jspack.Unpack('H', bytes);
+		
+		console.log(ntest);
+		ndata.push(ntest);
+	}
+
+	// console.log('ndata');
+	// console.log(ndata);
+	return ndata;
 }
 
 function getGyroData() {
   console.log('getGyroData');
-  console.log(gyroData);
+  // console.log(gyroData);
 
-  var degs = gyroData.slice(16, 17);
-  var check = gyroData.slice(15, 16);
-
-  degs = ((degs[0] & 0xff) << 8) | (degs[1] & 0xff);
-  check = ((check[0] & 0xff) << 8) | (check[1] & 0xff);
-  // degs = degs.readInt16BE();
-  // check = check.readInt16BE();
-  console.log('Check :', check);
-  console.log('Degs :', degs);
-
-  if (check == 1) {
-    return degs;
-  } else if (check == 0) {
-    return (degs - 200);
-  } else {
-    console.log('Probleme Gyro');
-  }
+  var test2 = [gyroData.slice(16, 17)];
+  var test = [gyroData.slice(15, 16)];
+  bytes = test.concat(test2);
+	var ntest = jspack.jspack.Unpack('H', bytes);
+	ntest = Math.abs(ntest[0] * 0.02/28);
+	if((ntest<0.4) && (ntest>0.2)){
+		return ntest;
+	} else {
+		return 0.2;
+	}
 }
 
 function turnBackWardLeft() {
 	console.log('turnBackWardLeft')
-  trameMotor = '\x4e' + '\x41' + '\x49' + '\x4f' + '\x30' + '\x31' + '\x01' + '\x00' + '\x00' + '\x00' + '\x02' + '\xc1' + '\x81' + '\x00' + '\x00' + '\x00' + '\x00';
+	trameMotor = '\x4e' + '\x41' + '\x49' + '\x4f' + '\x30' + '\x31' + '\x01' + '\x00' + '\x00' + '\x00' + '\x02' + '\xc1' + '\x81' + '\x00' + '\x00' + '\x00' + '\x00';
 	nmove(n_motor, trameMotor);
 }
 
 function turnBackWardRight() {
 	console.log('turnBackWardRight')
-  trameMotor = '\x4e' + '\x41' + '\x49' + '\x4f' + '\x30' + '\x31' + '\x01' + '\x00' + '\x00' + '\x00' + '\x02' + '\x81' + '\xc1' + '\x00' + '\x00' + '\x00' + '\x00';
+	trameMotor = '\x4e' + '\x41' + '\x49' + '\x4f' + '\x30' + '\x31' + '\x01' + '\x00' + '\x00' + '\x00' + '\x02' + '\x81' + '\xc1' + '\x00' + '\x00' + '\x00' + '\x00';
 	nmove(n_motor, trameMotor);
 }
 
